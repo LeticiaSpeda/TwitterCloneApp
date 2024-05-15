63ab86f85e5b684e4e941677cce08fe63b2e4886
Leticia <leticiaspeda@gmail.com>
2024-05-15 08:35:41 -0300

    ✨: create tabBar and created separation of file folders

---
+++ TwitterCloneApp/Controller/MainTabBarControllerViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class MainTabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+    
+}
