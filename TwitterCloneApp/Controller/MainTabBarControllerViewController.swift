07d95385ea6e06f3e161a448177229af09c0fa8d
Leticia <leticiaspeda@gmail.com>
2024-06-03 21:47:42 -0300

    ♻️ : refactor code to button

--- TwitterCloneApp/Controller/MainTabBarControllerViewController.swift
+++ TwitterCloneApp/Controller/MainTabBarControllerViewController.swift
@@ -1,13 +1,12 @@
 import UIKit
 
 class MainTabBarControllerViewController: UITabBarController {
-
+    
     let actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .white
         button.backgroundColor = .blue
         button.setImage(UIImage(named: "newTweet"), for: .normal)
-        button.layer.cornerRadius = 56/2
         return button
     }()
     
@@ -19,11 +18,9 @@ class MainTabBarControllerViewController: UITabBarController {
     
     func configureUI() {
         view.addSubview(actionButton)
-        actionButton.translatesAutoresizingMaskIntoConstraints = false
-        actionButton.heightAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.widthAnchor.constraint(equalToConstant: 56).isActive = true
-        actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -64).isActive = true
-        actionButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.layer.cornerRadius = 56/2
     }
     
     func configureViewControllers() {
